6638b2c352e6f486448fc1f964b7c52a0b550ed7
Stanley <krzysiekstachon@gmail.com>
2018-05-20 13:31:28 +0200

    NowyCommit

--- LearningC/main.c
+++ LearningC/main.c
@@ -3,6 +3,23 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char my_strings[50];
+    scanf("%s", my_strings);
+
+    //fgets(my_strings, 50, stdin);
+
+
+    FILE * file_h = fopen("Output.txt","a");
+    if(file_h == NULL) printf("ERROR");
+    fprintf(file_h,"%s", my_strings);
+
+    int i = strcmp(my_strings, "aaa");
+    printf("Dlugosc: %d\n",i);
+
+
+
+
+
+    printf("%s", my_strings);
     return 0;
 }
